4036d1ae73beb8959d55db53edf524aa0d36a399
phong <doanphong645gmailcom>
2023-11-04 11:35:42 +0700

    them giohang, tintuc, dangki,dangnhap

---
+++ View/dangki.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="col-xl-6 col-lg-6 col-md-8">
+				<div class="box_account">
+					<h3 class="new_client"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Khách hàng mới</font></font></h3> <small class="float-right pt-2"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">* Phần bắt buộc</font></font></small>
+					<div class="form_container">
+						<div class="form-group">
+							<input type="email" class="form-control" name="email" id="email_2" placeholder="E-mail*">
+						</div>
+						<div class="form-group">
+							<input type="password" class="form-control" name="password_in_2" id="password_in_2" value="" placeholder="Mật khẩu*">
+						</div>
+						<hr>
+						<div class="form-group">
+							<label class="container_radio" style="display: inline-block; margin-right: 15px;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Riêng tư
+								</font></font><input type="radio" name="client_type" checked="" value="private">
+								<span class="checkmark"></span>
+							</label>
+							<label class="container_radio" style="display: inline-block;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Công ty
+								</font></font><input type="radio" name="client_type" value="company">
+								<span class="checkmark"></span>
+							</label>
+						</div>
+						<div class="private box">
+							<div class="row no-gutters">
+								<div class="col-6 pr-1">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Tên*">
+									</div>
+								</div>
+								<div class="col-6 pl-1">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Họ*">
+									</div>
+								</div>
+								<div class="col-12">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Địa chỉ đầy đủ*">
+									</div>
+								</div>
+							</div>
+							<!-- /row -->
+							<div class="row no-gutters">
+								<div class="col-6 pr-1">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Thành phố*">
+									</div>
+								</div>
+								<div class="col-6 pl-1">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Mã bưu điện*">
+									</div>
+								</div>
+							</div>
+							<!-- /row -->
+							
+							<div class="row no-gutters">
+								<div class="col-6 pr-1">
+									<div class="form-group">
+										<div class="custom-select-form">
+											<select class="wide add_bottom_10" name="country" id="country" style="display: none;">
+													<option value="" selected=""><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Quốc gia*</font></font></option>
+													<option value="Europe"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Châu Âu</font></font></option>
+													<option value="United states"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Hoa Kỳ</font></font></option>
+													<option value="Asia"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Châu Á</font></font></option>
+											</select><div class="nice-select wide add_bottom_10" tabindex="0"><span class="current"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Quốc gia*</font></font></span><ul class="list"><li data-value="" class="option selected"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Quốc gia*</font></font></li><li data-value="Europe" class="option"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Châu Âu</font></font></li><li data-value="United states" class="option"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Hoa Kỳ</font></font></li><li data-value="Asia" class="option"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Châu Á</font></font></li></ul></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-6 pl-1">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Điện thoại *">
+									</div>
+								</div>
+							</div>
+							<!-- /row -->
+							
+						</div>
+						<!-- /private -->
+						<div class="company box" style="display: none;">
+							<div class="row no-gutters">
+								<div class="col-12">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Tên công ty*">
+									</div>
+								</div>
+								<div class="col-12">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Địa chỉ đầy đủ">
+									</div>
+								</div>
+							</div>
+							<!-- /row -->
+							<div class="row no-gutters">
+								<div class="col-6 pr-1">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Thành phố*">
+									</div>
+								</div>
+								<div class="col-6 pl-1">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Mã bưu điện*">
+									</div>
+								</div>
+							</div>
+							<!-- /row -->
+							<div class="row no-gutters">
+								<div class="col-6 pr-1">
+									<div class="form-group">
+										<div class="custom-select-form">
+											<select class="wide add_bottom_10" name="country" id="country_2" style="display: none;">
+													<option value="" selected=""><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Quốc gia*</font></font></option>
+													<option value="Europe"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Châu Âu</font></font></option>
+													<option value="United states"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Hoa Kỳ</font></font></option>
+													<option value="Asia"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Châu Á</font></font></option>
+											</select><div class="nice-select wide add_bottom_10" tabindex="0"><span class="current"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Quốc gia*</font></font></span><ul class="list"><li data-value="" class="option selected"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Quốc gia*</font></font></li><li data-value="Europe" class="option"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Châu Âu</font></font></li><li data-value="United states" class="option"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Hoa Kỳ</font></font></li><li data-value="Asia" class="option"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Châu Á</font></font></li></ul></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-6 pl-1">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Điện thoại *">
+									</div>
+								</div>
+							</div>
+							<!-- /row -->
+						</div>
+						<!-- /company -->
+						<hr>
+						<div class="form-group">
+							<label class="container_check"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Chấp nhận </font></font><a href="#0"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Điều khoản và điều kiện</font></font></a>
+								<input type="checkbox">
+								<span class="checkmark"></span>
+							</label>
+						</div>
+						<div class="text-center"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"><input type="submit" value="Đăng ký" class="btn_1 full-width"></font></font></div>
+					</div>
+					<!-- /form_container -->
+				</div>
+				<!-- /box_account -->
+			</div>
+</body>
+</html>
